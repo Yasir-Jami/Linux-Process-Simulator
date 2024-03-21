5957f0682ce665609511fd3cc5cbc577135de157
Salvale <45802318+Salvale@users.noreply.github.com>
2024-03-21 12:55:41 -0600

    Add files via upload

--- lab9/main.c
+++ lab9/main.c
@@ -6,66 +6,32 @@
 #include <dirent.h>
 #include "findme.h"
 
+void sprint(char* pathname, char* type, int);
+
+
+
 int main(int argc, char* argv[]){
 	// Require pathname
 	if (argc < 2){
 		printf("Usage: findme --type [arg] --name [arg] --user [arg] --maxdepth [arg]\n");
 		exit(EXIT_FAILURE);
 	}
-	
+
 	// Arg defaults
 	//char* pathname = argv[1];; // current directory
-	
+
 	char* pathname = ".";
 	char* type = "f"; // File type: f - reg, d - dir, etc. - set to 0 by default to look for all file types
+
 	/*
 	char* name = "0"; // Specified filename
-	char* user = "jamiy2"; // User name 
-	int maxdepth = 1; // Height of directory search to look through
-	*/
+	char* user = "jamiy2"; // User name*/
+	int maxdepth = 10; // Height of directory search to look through
 
-	//parseCommands(argc, argv, &type, &name, &user, &maxdepth);
-	
-	// Check for path validity
-	struct stat buf; // stat.h pointer used to determine a file's stats 
-	DIR *dir; // Pointer to origin directory - provided by user or current directory by default
-	DIR *currentdir; // Pointer to current directory being examined	
-	struct dirent *file; // Pointer to current file being examined
-	char fileloc[100]; // Name of file being examined
-	int argtype = getFileArgType(*type); // int representing file type
 
-	// Open origin path
-	dir = opendir(pathname);
-	if (dir == NULL){
-		printf("Not a valid directory. Closing program.\n");
-		exit(EXIT_FAILURE);
-	}
+	sprint(pathname, type,maxdepth);
 
-	/*
-	readdir(DIR *) - reads the content of a directory and returns an object of type dirent (struct)	
-	*/
 
-	// Need to check file type before printing
-	// Search through all directories according to specified maxdepth; set to 255 by default
-	// Recursive search - search through the first encounter directory, print all contents, go to next directory, print, etc.
-	while ((file = readdir(dir))){
-		if ((strcmp(file->d_name, ".") == 0) || (strcmp(file->d_name, "..") == 0)){
-			continue;
-		} 
-		// Need to print file location in the form (origin_path_of_directory)/(FILE)
-		// So keep appending file directories until reaching the file
-		strcpy(fileloc, pathname);
-	        strncat(fileloc, "/", 2);
-		strncat(fileloc, file->d_name, 100);
-		stat(fileloc, &buf);
-	
-		// If chosen file's type matches the chosen -type argument or if there is no -type arg specified, print the path
-		if (getFileType(fileloc) == argtype || argtype == -1){
-			// Point to file - goes to next file if file is not of the type being examined
-			printf("%s\n", fileloc);
-		}
-	}
-	closedir(dir);	
 
 	/*
 	printf("\nCurrent type: %s\n", type);
@@ -76,3 +42,41 @@ int main(int argc, char* argv[]){
 
 	return 0;
 }
+
+void sprint(char* pathname, char* type, int depth){
+	if (depth == 0) {
+		return;
+	}
+	struct stat buf;
+	DIR *dir;
+//	DIR *currentdir;
+	struct dirent *file;
+	char fileloc[100];
+	int argtype = getFileArgType(*type);
+	dir = opendir(pathname);
+
+	if (dir == NULL) {
+		printf("Not a valid dir. Closing program\n");
+		exit(EXIT_FAILURE);
+	}
+
+	while((file = readdir(dir))) {
+		if ((strcmp(file->d_name,".") == 0) || (strcmp(file->d_name, "..")==0)) {
+			continue;
+		}
+		strcpy(fileloc,pathname);
+		strncat(fileloc,"/",2);
+		strncat(fileloc, file->d_name, 100);
+		stat(fileloc,&buf);
+		if (getFileType(fileloc)==1) {
+			sprint(fileloc, type, depth-1);
+		}
+		if (getFileType(fileloc) == argtype || argtype == -1) {
+			printf("%s\n", fileloc);
+		}
+
+	}
+	closedir(dir);
+}
+
+
